f118c891a708de515e137714213c3238fcfe517f
Jeremy Gathing <bigguy@Jeremys-Mac-mini.local>
2021-02-11 18:40:00 -0600

    Further updates

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,9 +2,11 @@ package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,15 +15,25 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
+
+
     @GetMapping
     public String displayAlLEvents(Model model){
-        List<String> events = new ArrayList<>();
-        events.add("Juneteenth");
-        events.add("Cinco de Mayo");
-        events.add("Soul Roll");
-        events.add("Rolling in the Carolinas");
         model.addAttribute("events", events);
         return "events/index";
 
     }
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm(){
+        return "events/create";
+    }
+    // lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName){
+        events.add(eventName);
+        return "redirect:";
+    }
+    // lives at /events/create
 }
